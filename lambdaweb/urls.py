bf290508ed94b252c8c1a496a66817d0471453d1
murych <murychkun@yahoo.com>
2017-01-08 02:05:02 +0300

    Начинается переход на Django CMS #15
    - удален весь старый проект :'(
    - запущен новый проект с cms
    - TODO: вернуть стили и главную страницу

--- lambdaweb/urls.py
+++ lambdaweb/urls.py
@@ -1,21 +1,29 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-from . import views
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.conf import settings
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
 
+from cms.sitemaps import CMSSitemap
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.i18n import i18n_patterns
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+admin.autodiscover()
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
-	url(r'^admin/', include(admin.site.urls)),
-	url(r'^markdown/', include('django_markdown.urls')),
-	url(r'^registry/', include('registry.urls')),
-	url(r'^partners/$', views.partners, name="partners"),
-	url(r'^projects/$', views.projects, name="projects"),
-	url(r'^members/$', views.members, name="members"),
-	url(r'^events/$', views.events, name="events"),
+    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap',
+        {'sitemaps': {'cmspages': CMSSitemap}}),
+    url(r'^select2/', include('django_select2.urls')),
 ]
 
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += i18n_patterns('',
+    url(r'^admin/', include(admin.site.urls)),  # NOQA
+    url(r'^', include('cms.urls')),
+)
+
+# This is only needed when using runserver.
+if settings.DEBUG:
+    urlpatterns = [
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+        ] + staticfiles_urlpatterns() + urlpatterns
